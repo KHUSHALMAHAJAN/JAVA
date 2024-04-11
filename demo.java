6304d9bfe97de1928839e9f8575a790fbc05e15a
KHUSHALMAHAJAN <khushalmahajan303@gmail.com>
2024-04-11 09:21:41 +0530

    add new file

---
+++ demo.java
@@ -0,0 +1,8 @@
+import mypack.*;
+class demo{
+   public static void main(String args[]){
+	//mypack.start obj = new mypack.start();
+	start obj = new start();
+	obj.display();
+     }
+}
---
+++ start.java
@@ -0,0 +1,9 @@
+package mypack;
+public class start{
+   public void display(){
+  	System.out.println("they in display () class");
+       }
+   public static void main(String args[]){
+	System.out.println("they are main function");
+     }
+}
